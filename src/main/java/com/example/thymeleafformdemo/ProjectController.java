98dbfa66b3da0fd50f3782ff2b0d809678f18daf
John Kyaw <jjpyae@gmail.com>
2024-02-14 14:21:54 -0800

    transfered files

--- src/main/java/com/example/thymeleafformdemo/ProjectController.java
+++ src/main/java/com/example/thymeleafformdemo/ProjectController.java
@@ -6,6 +6,9 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 
+import java.util.ArrayList;
+import java.util.List;
+
 // sourced from from:
 //
 // https://attacomsian.com/blog/spring-boot-thymeleaf-form-handling
@@ -13,11 +16,13 @@ import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class ProjectController {
+    List<Project> projectList = new ArrayList<>();
 
     @GetMapping("/create-project")
     public String createProjectForm(Model model) {
 
         model.addAttribute("project", new Project());
+        model.addAttribute("projectList", projectList);
         return "create-project";
     }
 
@@ -25,6 +30,9 @@ public class ProjectController {
     public String saveProjectSubmission(@ModelAttribute Project project) {
 
         // TODO: save project in DB here
+        projectList.add(project);
+        System.out.println(projectList.size());
+
 
         return "result";
     }
